eccadd6660e0b19ef6bd8c85d8bfb9825f43cde0
Micah Martin <micahlmartin@gmail.com>
2015-04-03 14:25:36 -0400

    final fixes

--- test/boomtown.js
+++ test/boomtown.js
@@ -11,21 +11,19 @@ lab.test('test', function(done) {
     secret: ''
   }).then(function(client) {
     client.registerLead({
-      Email: "micahlmartin@gmail.com",
-      FirstName: "Micah",
-      LastName: "Martin",
-      Phone: "(555) 555-5555",
-      Comment: "This lead is awesome",
-      PropertyAddress: "123 Main Street, Charleston, SC 29403"
+      "AgentId": 1,
+      "Email":"john.doe@example.com",
+      "FirstName":"John",
+      "LastName":"Doe",
+      "Phone":"(555)555-5555",
+      "Comment":"my comment",
+      "PropertyAddress":"123 Main St, Charleston, SC 29403",
     }).then(function(response) {
-      console.log(response);
       done();
     }).fail(function(err) {
-      console.log(err);
       done();
     });
   }).fail(function(err) {
-    console.log(err);
     done();
   });
 
